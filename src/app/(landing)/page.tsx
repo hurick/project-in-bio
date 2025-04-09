dd6f1c7d11b36a36caed167ae749653edee75088
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2025-04-09 20:35:20 -0300

    fix: adjust gap spacing in landing page layout for improved visual consistency

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,7 +2,7 @@ import { Header, Hero } from "./components";
 
 export default function Home() {
   return (
-    <div className="flex flex-col max-w-7xl mx-auto gap-30">
+    <div className="flex flex-col max-w-7xl mx-auto gap-20">
       <Header />
       <Hero />
 
