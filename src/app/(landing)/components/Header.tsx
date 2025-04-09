748c98371f00459099bc5df7ca9c41bcbfad427f
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2025-04-09 20:38:34 -0300

    fix: specify width and height attributes for logo image in Header component to improve layout consistency

--- src/app/(landing)/components/Header.tsx
+++ src/app/(landing)/components/Header.tsx
@@ -5,7 +5,7 @@ import { imgLogo } from "@/app/static/images";
 export function Header() {
   return (
     <header className="flex items-center justify-between py-10">
-      <img src={imgLogo.src} alt="ProjectinBio logo" />
+      <img src={imgLogo.src} alt="ProjectinBio logo" width={187} height={32} />
 
       <div className="flex items-center gap-4">
         <Button>My page</Button>
