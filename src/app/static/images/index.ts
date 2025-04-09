b871c3f57a80475098f0935fc30a16cb005ec777
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2025-04-09 19:28:24 -0300

    feat: replace outdated image formats with WebP versions for improved performance and reduce file size

--- src/app/static/images/index.ts
+++ src/app/static/images/index.ts
@@ -1,5 +1,5 @@
-import imgProject01 from "./project-01.png";
-import imgProject02 from "./project-02.png";
-import imgHurick from "./hurick-01.jpg";
+import imgProject01 from "./project-01.webp";
+import imgProject02 from "./project-02.webp";
+import imgHurick from "./hurick-01.webp";
 
 export { imgProject01, imgProject02, imgHurick };
