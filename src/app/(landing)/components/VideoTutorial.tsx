813b347dca35dc37bd35bb0d232d4fd7d46aacbc
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2025-04-10 00:10:33 -0300

    feat: implement VideoTutorial component with placeholder image and play button, add tutorial placeholder image to static assets

--- src/app/(landing)/components/VideoTutorial.tsx
+++ src/app/(landing)/components/VideoTutorial.tsx
@@ -1,3 +1,17 @@
+import { Play } from "lucide-react";
+
+import { imgTutorialPlaceholder } from "@/app/static/images";
+
 export function VideoTutorial() {
-  return <div>VideoTutorial</div>;
+  return (
+    <div className="relative flex items-center justify-center border border-border-primary aspect-video rounded-xl overflow-hidden">
+      <img
+        className="w-full h-full object-cover rounded-xl brightness-50"
+        src={imgTutorialPlaceholder.src}
+        alt="Video tutorial thumbnail placeholder"
+      />
+
+      <Play className="p-1 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-accent-purple size-16 hover:scale-150 transition-transform cursor-pointer" />
+    </div>
+  );
 }
--- src/app/static/images/index.ts
+++ src/app/static/images/index.ts
@@ -1,7 +1,14 @@
 import imgProject01 from "./project-01.webp";
 import imgProject02 from "./project-02.webp";
 import imgHurick from "./hurick-01.webp";
+import imgTutorialPlaceholder from "./tutorial-placeholder.webp";
 
 import imgLogo from "./logo-projectinbio.svg";
 
-export { imgProject01, imgProject02, imgHurick, imgLogo };
+export {
+  imgProject01,
+  imgProject02,
+  imgHurick,
+  imgLogo,
+  imgTutorialPlaceholder,
+};
