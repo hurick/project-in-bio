75e192f4bc7e11a63de81eec3d65227d9679e7d1
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2025-04-12 18:44:00 -0300

    feat: implement link creation form in CreatePage with input and button for user interaction

--- src/app/(pages)/create/page.tsx
+++ src/app/(pages)/create/page.tsx
@@ -1,9 +1,34 @@
-import { Header } from "@/app/components";
+import { Rocket } from "lucide-react";
+
+import { Button, Header, Input } from "@/app/components";
 
 export default function CreatePage() {
   return (
-    <div className="flex flex-col gap-20 pb-20">
+    <div className="flex flex-col">
       <Header />
+
+      <div className="h-[calc(100vh-260px)] flex flex-col items-center justify-center gap-10">
+        <div className="flex items-center gap-4 text-white">
+          <h1 className="font-bold text-4xl">Choose your link</h1>
+          <Rocket className="size-10" />
+        </div>
+
+        <form className="flex items-center gap-2">
+          <span className="text-white">projectinbio.com/</span>
+          <Input
+            name="your-link"
+            placeholder="your-custom-link-here"
+            inputClassName="min-w-96"
+          />
+          <Button type="submit" className="min-w-32">
+            Create
+          </Button>
+        </form>
+
+        <div>
+          <span className="text-accent-pink">Example error</span>
+        </div>
+      </div>
     </div>
   );
 }
